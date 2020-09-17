626df78a853e6ca6581540fd3ee88aef15d6b701
Daniil Krasnov <dk98126@yandex.ru>
2020-09-17 22:23:42 +0300

    initial commit

---
+++ src/test/java/com/dk98126/mireabankapp/MireaBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk98126.mireabankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MireaBankAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
